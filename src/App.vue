9f63eacfa457137f37cde3ca0a80667b874f6ecb
Princeraul <suliangprinceraul@gmail.com>
2021-09-26 18:39:05 +0800

    footer 组件编写未完

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <Header />
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Footer />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/header.vue'
+import Footer from './components/footer.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    Header
+    Header,
+    Footer
   }
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer>
+    <div class="panel">
+      <div class="contact">
+        <h3>消费者服务</h3>
+        <div class="contact-info">
+          <a>
+            防伪查询
+            <div class="qrcode">
+              <img src="../favicon.icoassets/logo.jpg" alt="防伪查询"><br/>请使用微信扫一扫
+            </div>
+          </a>
+          <a>
+            售后服务
+            <div class="qrcode">028-65232887(工作日9:00-18:00)</div>
+          </a>
+        </div>
+      </div>
+      <div class="address"></div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+ name: 'Footer'
+}
+</script>
+
+<style>
+
+</style>
