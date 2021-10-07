4cbb4f95c3bea4ec1571a4e60f77e486dc5865a6
xiaoxiaosl <398528895@qq.com>
2021-10-07 22:34:55 +0800

    cd

---
+++ src/components/product.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="product panel-layout">
+    <el-carousel :height="bannerHeight + 'px'" :interval="50000" :autoplay="flase" arrow="always" indicator-position="none">
+      <el-carousel-item :key="index" pause-on-hover="true">
+          <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+              <li>6</li>
+          </ul>
+      </el-carousel-item>
+      <el-carousel-item :key="index" pause-on-hover="true">
+          <ul>
+              <li>5</li>
+              <li>6</li>
+              <li>7</li>
+       
+          </ul>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bannerItem: [
+        {
+          imgUrl: require("@/static/banner1.jpg"),
+          path: "/about",
+        },
+        {
+          imgUrl: require("@/static/banner2.jpg"),
+          path: "/about",
+        },
+        {
+          imgUrl: require("@/static/banner3.jpg"),
+          path: "/about",
+        },
+        {
+          imgUrl: require("@/static/banner4.jpg"),
+          path: "/about",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+ul {
+    width: 1350px;
+}
+ul li { 
+    float: left;
+    width: 375px;
+    height: 375px;
+}
+
+.product .el-carousel__item {
+    width: 375px;
+}
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,17 +1,22 @@
 <template>
   <Header />
-  <el-carousel :height="bannerHeight +'px'"  arrow="always" :interval="500000">
-    <el-carousel-item v-for="(item, index) in bannerItem"  :key="index" pause-on-hover="true">
-      <img :src="item.imgUrl" @click="PageJump(item.path)">
-    </el-carousel-item> 
+  <el-carousel :height="bannerHeight + 'px'" :interval="50000">
+    <el-carousel-item
+      v-for="(item, index) in bannerItem"
+      :key="index"
+      pause-on-hover="true"
+    >
+      <img :src="item.imgUrl" @click="PageJump(item.path)" />
+    </el-carousel-item>
   </el-carousel>
+  <Product />
   <div class="product-details">
-    <h2 class="detail-title">{{productTxt}}</h2>
+    <h2 class="detail-title">{{ productTxt }}</h2>
     <div class="detail-layout">
       <div class="detail-img"><img :src="detailImg" /></div>
       <div class="detail-body">
         <div v-for="(item, index) in details" :key="index">
-          <h5>{{item.title}}</h5>
+          <h5>{{ item.title }}</h5>
           <p v-html="item.content"></p>
         </div>
       </div>
@@ -21,73 +26,76 @@
 </template>
 
 <script>
-import Header from '@/components/header.vue'
-import Footer from '@/components/footer.vue'
+import Header from "@/components/header.vue";
+import Footer from "@/components/footer.vue";
+import Product from '@/components/product';
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     Header,
     Footer,
+    Product,
   },
   data() {
     return {
-      bannerHeight: '',
-      screenHeight: '',
-      detailImg: require('@/static/1.jpg'),
+      bannerHeight: "",
+      screenHeight: "",
+      detailImg: require("@/static/1.jpg"),
       details: [
         {
-          title: '以桂花为本，专业修复肌肤问题',
-          content: '10年桂花系统研究,至简配方模式<br/>为更多人特别是婴幼儿、孕妈妈等特殊群体<br/>带来天然更有效的肌肤修护体验'
+          title: "以桂花为本，专业修复肌肤问题",
+          content:
+            "10年桂花系统研究,至简配方模式<br/>为更多人特别是婴幼儿、孕妈妈等特殊群体<br/>带来天然更有效的肌肤修护体验",
         },
         {
-          title: '专利止痒修护成分：桂花Osthin™',
-          content: '源自新鲜天然桂花萃取，能有效舒缓、呵护脆弱肌肤，<br/> 提升肌肤自愈力。<br/> 每1000g新鲜桂花仅能萃取0.02g Osthin™<br />  仅需一点，即刻修护'
-          
-        }
+          title: "专利止痒修护成分：桂花Osthin™",
+          content:
+            "源自新鲜天然桂花萃取，能有效舒缓、呵护脆弱肌肤，<br/> 提升肌肤自愈力。<br/> 每1000g新鲜桂花仅能萃取0.02g Osthin™<br />  仅需一点，即刻修护",
+        },
       ],
-      productTxt: '以桂花为本，专业修复肌肤问题',
+      productTxt: "以桂花为本，专业修复肌肤问题",
       bannerItem: [
         {
-          imgUrl: require('@/static/banner1.jpg'),
-          path: '/about' 
+          imgUrl: require("@/static/banner1.jpg"),
+          path: "/about",
         },
         {
-          imgUrl: require('@/static/banner2.jpg'),
-          path: '/about' 
+          imgUrl: require("@/static/banner2.jpg"),
+          path: "/about",
         },
         {
-          imgUrl: require('@/static/banner3.jpg'),
-          path: '/about' 
+          imgUrl: require("@/static/banner3.jpg"),
+          path: "/about",
         },
         {
-          imgUrl: require('@/static/banner4.jpg'),
-          path: '/about' 
-        }
-      ]
-    }
+          imgUrl: require("@/static/banner4.jpg"),
+          path: "/about",
+        },
+      ],
+    };
   },
   mounted() {
     this.screenWidth = window.innerWidth;
     this.setSize();
-    window.onresize = ()=> {
+    window.onresize = () => {
       this.screenWidth = window.innerWidth;
       this.setSize();
-    }
+    };
   },
   methods: {
     setSize() {
-      this.bannerHeight = 575 / 1841 * this.screenWidth
+      this.bannerHeight = (575 / 1841) * this.screenWidth;
     },
     PageJump(link) {
-       if(link.indexOf('https') != '-1') {
-        window.location.href = link
-      }else {
-        this.$router.push(link)
+      if (link.indexOf("https") != "-1") {
+        window.location.href = link;
+      } else {
+        this.$router.push(link);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -103,7 +111,7 @@ export default {
   position: relative;
   text-align: center;
   white-space: nowrap;
-  
+
   &:after {
     background-color: #007acd;
     content: "";
@@ -128,7 +136,7 @@ export default {
   padding-top: 50px;
 
   &:after {
-    content: '';
+    content: "";
     position: absolute;
     top: 0;
     right: -100%;
@@ -136,11 +144,10 @@ export default {
     height: 100%;
     background: #fff;
   }
-
 }
 .detail-img {
   position: absolute;
-  left: -308px; 
+  left: -308px;
 }
 .detail-body {
   float: right;
